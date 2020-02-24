8eeb815a54242a73c436912a866858e1620f84c4
Patrik Lindström <patrik@jumpingskunk.com>
2020-02-24 16:15:35 +0200

    Update comments in ScreenPainter.java

--- src/engine/core/ScreenPainter.java
+++ src/engine/core/ScreenPainter.java
@@ -4,6 +4,9 @@ import java.awt.Graphics2D;
 
 /**
  * Minimalist interface to connect Application to Screen.
+ * TODO: refactor this interface such that either its name
+ * follows Java conventions (i.e. it is in the form of an
+ * adjective) or change it into an abstract class.
  */
 public interface ScreenPainter {
 
